27a9477e5bb8351eee96db5312ecd4d529faebbb
sagivStekolshik <sagivstekolshik@Sagivs-MacBook-Pro.local>
2020-04-24 00:07:52 +0300

    lint on app

--- reactNavigationDrawerLTRBug/App.js
+++ reactNavigationDrawerLTRBug/App.js
@@ -1,45 +1,38 @@
 import React, {useState} from 'react';
-import { Button,StyleSheet, View } from 'react-native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { NavigationContainer } from '@react-navigation/native';
-
-
+import {Button, StyleSheet, View} from 'react-native';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import {NavigationContainer} from '@react-navigation/native';
 
 const Drawer = createDrawerNavigator();
 
 const App = () => {
-  const [drawerPosition, setDrawerPosition] = useState('left')
+  const [drawerPosition, setDrawerPosition] = useState('left');
 
-  const HomeScreen = ({ navigation }) => {
+  const HomeScreen = ({navigation}) => {
     return (
       <View style={styles.container}>
-        <Button 
+        <Button
           onPress={() => {
-            setDrawerPosition('right')
-            navigation.openDrawer()
-          }} 
-          title="Create bug" 
+            setDrawerPosition('right');
+            navigation.openDrawer();
+          }}
+          title="Create bug"
         />
-        
       </View>
     );
-  }
+  };
 
   return (
     <NavigationContainer>
-      <Drawer.Navigator 
-        drawerPosition={drawerPosition}
-        initialRouteName="Home">
+      <Drawer.Navigator drawerPosition={drawerPosition} initialRouteName="Home">
         <Drawer.Screen name="Home" component={HomeScreen} />
-        
       </Drawer.Navigator>
     </NavigationContainer>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: { flex: 1, alignItems: 'center', justifyContent: 'center' }
-})
-
+  container: {flex: 1, alignItems: 'center', justifyContent: 'center'},
+});
 
 export default App;
