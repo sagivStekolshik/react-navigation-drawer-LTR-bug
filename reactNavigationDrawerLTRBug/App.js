0c8e5300b4521dfd53faf9719e3b124c379f304c
sagivStekolshik <sagivstekolshik@Sagivs-MacBook-Pro.local>
2020-04-23 23:59:12 +0300

    move HomeScreen inside App and make it a single button

--- reactNavigationDrawerLTRBug/App.js
+++ reactNavigationDrawerLTRBug/App.js
@@ -3,32 +3,34 @@ import { Button,StyleSheet, View } from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 
-const HomeScreen = ({ navigation, setDrawerPosition }) => {
-  return (
-    <View style={styles.container}>
-      <Button
-        onPress={() => navigation.openDrawer()}
-        title="Open drawer"
-      />
-      <View style={{flexDirection: 'row'}}>
-        <Button onPress={() => setDrawerPosition('left')} title="Set position left" />
-        <Button onPress={() => setDrawerPosition('right')} title="Set position right" />
-      </View>
-    </View>
-  );
-}
+
 
 const Drawer = createDrawerNavigator();
 
 const App = () => {
   const [drawerPosition, setDrawerPosition] = useState('left')
 
+  const HomeScreen = ({ navigation }) => {
+    return (
+      <View style={styles.container}>
+        <Button 
+          onPress={() => {
+            setDrawerPosition('right')
+            navigation.openDrawer()
+          }} 
+          title="Create bug" 
+        />
+        
+      </View>
+    );
+  }
+
   return (
     <NavigationContainer>
       <Drawer.Navigator 
         drawerPosition={drawerPosition}
         initialRouteName="Home">
-        <Drawer.Screen name="Home" component={(props) => <HomeScreen {...props} setDrawerPosition={setDrawerPosition}/>} />
+        <Drawer.Screen name="Home" component={HomeScreen} />
         
       </Drawer.Navigator>
     </NavigationContainer>
